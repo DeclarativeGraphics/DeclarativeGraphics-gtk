60a50e64736ae6b95e2590a7f9de3b86d510f350
Philipp Krüger <matheus.dev@gmail.com>
2015-02-13 13:46:24 +0100

    MOOOORE KEEEEEEYS

--- Graphics/Declarative/Gtk/KeyboardInput.hs
+++ Graphics/Declarative/Gtk/KeyboardInput.hs
@@ -7,14 +7,17 @@ import qualified Graphics.UI.Gtk as G
 
 data Key = Letter Char
          | Special SpecialKey
-         deriving (Show, Eq)
+           deriving (Show, Eq, Ord)
 
-data SpecialKey = Return | Escape | Backspace | Shift
+data SpecialKey = Return | Escape | Backspace | Delete | Insert | Shift
                 | ArrLeft | ArrUp | ArrRight | ArrDown
                 | Home | End | PageUp | PageDown
-                | DeadCircumflex
+                | DeadCircumflex | DeadGrave | DeadAcute
                 | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10 | F11 | F12
-                deriving (Show, Eq)
+                | Control | ControlRight | ShiftRight
+                | CapsLock | Tab | Alt | AltRight
+                | AltGr | Menu
+                deriving (Show, Eq, Ord)
 
 
 keyboardInputFromGdk :: G.KeyVal -> Maybe Key
@@ -25,6 +28,8 @@ keyboardInputFromGdk k = maybe parseSpecialKey parseLetter (G.keyToChar k)
       "Return"          -> Just $ Special Return
       "Escape"          -> Just $ Special Escape
       "BackSpace"       -> Just $ Special Backspace
+      "Delete"          -> Just $ Special Delete
+      "Insert"          -> Just $ Special Insert
       "Shift_L"         -> Just $ Special Shift
       "Left"            -> Just $ Special ArrLeft
       "Right"           -> Just $ Special ArrRight
@@ -35,6 +40,8 @@ keyboardInputFromGdk k = maybe parseSpecialKey parseLetter (G.keyToChar k)
       "Page_Up"         -> Just $ Special PageUp
       "Page_Down"       -> Just $ Special PageDown
       "dead_circumflex" -> Just $ Special DeadCircumflex
+      "dead_grave"      -> Just $ Special DeadGrave
+      "dead_acute"      -> Just $ Special DeadAcute
       "F1"              -> Just $ Special F1
       "F2"              -> Just $ Special F2
       "F3"              -> Just $ Special F3
@@ -47,4 +54,13 @@ keyboardInputFromGdk k = maybe parseSpecialKey parseLetter (G.keyToChar k)
       "F10"             -> Just $ Special F10
       "F11"             -> Just $ Special F11
       "F12"             -> Just $ Special F12
+      "Control_L"       -> Just $ Special Control
+      "Control_R"       -> Just $ Special ControlRight
+      "Shift_R"         -> Just $ Special ShiftRight
+      "Caps_Lock"       -> Just $ Special CapsLock
+      "Tab"             -> Just $ Special Tab
+      "Alt_L"           -> Just $ Special Alt
+      "Alt_R"           -> Just $ Special AltRight
+      "Menu"            -> Just $ Special Menu
+      "ISO_Level3_Shift"-> Just $ Special AltGr
       unknown     -> (trace ("Don't know how to interpret GDK key " ++ show unknown) Nothing)
